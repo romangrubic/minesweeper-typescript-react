f816aef155ebbaceb1fcb057e1660c6350f1dc6d
Roman Grubic <grubic.roman@gmail.com>
2020-08-14 20:52:33 +0100

    Dynamicly setting number of row and collumns for generating field

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.styles.scss';
 import NumberDisplay from '../number-display/number-display.component';
+import { generateCells } from '../../utils/utils';
+import Button from '../button/button.component';
 
 const App: React.FC = () => {
+    // Rows and columns for game
+    const rows: number = 9;
+    const columns: number = 9;
+
+    const [cells, setCells] = useState(generateCells(rows, columns));
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) =>
+            row.map((cell, colIndex) => (
+                <Button key={rowIndex + '-' + colIndex} />
+            ))
+        );
+    };
+
+    // Inline style to control grid size
+    const bodyGrid = {
+        display: 'grid',
+        gridTemplateRows: `repeat(${rows}, 1fr)`,
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+    };
+
+    // User input form for rows and columns
+
     return (
         <div className='App'>
             <div className='Header'>
@@ -14,7 +39,9 @@ const App: React.FC = () => {
                 </div>
                 <NumberDisplay value={23} />
             </div>
-            <div className='Body'>Body</div>
+            <div className='Body' style={bodyGrid}>
+                {renderCells()}
+            </div>
         </div>
     );
 };
