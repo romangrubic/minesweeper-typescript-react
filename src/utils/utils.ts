6e1d7054798232ff501a1d2ecf642c2fc70ee6a9
Roman Grubic <grubic.roman@gmail.com>
2020-08-15 13:54:12 +0100

    Added logic for generating field number equal to number of adjacent bombs.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -34,6 +34,77 @@ export const generateCells = (
             bombsPlaced++;
         }
     }
+    // Numbers for each cell depending on how many bombs in adjacent cells
+    for (let rowIndex = 0; rowIndex < rowNumber; rowIndex++) {
+        for (let columnIndex = 0; columnIndex < columnNumber; columnIndex++) {
+            const currentCell = cells[rowIndex][columnIndex];
+            if (currentCell.value === CellValue.bomb) {
+                continue;
+            }
+
+            let numberOfBombs = 0;
+
+            const topLeftBomb =
+                rowIndex > 0 && columnIndex > 0
+                    ? cells[rowIndex - 1][columnIndex - 1]
+                    : null;
+            const topBomb =
+                rowIndex > 0 ? cells[rowIndex - 1][columnIndex] : null;
+            const topRightBomb =
+                rowIndex > 0 && columnIndex < columnNumber
+                    ? cells[rowIndex - 1][columnIndex + 1]
+                    : null;
+            const leftBomb =
+                columnIndex > 0 ? cells[rowIndex][columnIndex - 1] : null;
+            const rightBomb =
+                columnIndex < columnNumber
+                    ? cells[rowIndex][columnIndex + 1]
+                    : null;
+            const bottomLeftBomb =
+                rowIndex < rowNumber - 1 && columnIndex > 0
+                    ? cells[rowIndex + 1][columnIndex - 1]
+                    : null;
+            const bottomBomb =
+                rowIndex < rowNumber - 1
+                    ? cells[rowIndex + 1][columnIndex]
+                    : null;
+            const bottomRightBomb =
+                rowIndex < rowNumber - 1 && columnIndex < rowNumber - 1
+                    ? cells[rowIndex + 1][columnIndex + 1]
+                    : null;
+
+            if (topLeftBomb && topLeftBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (topBomb && topBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (topRightBomb && topRightBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (leftBomb && leftBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (rightBomb && rightBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (bottomLeftBomb && bottomLeftBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (bottomBomb && bottomBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (bottomRightBomb && bottomRightBomb.value === CellValue.bomb) {
+                numberOfBombs++;
+            }
+            if (numberOfBombs > 0) {
+                cells[rowIndex][columnIndex] = {
+                    ...currentCell,
+                    value: numberOfBombs,
+                };
+            }
+        }
+    }
 
     return cells;
 };
