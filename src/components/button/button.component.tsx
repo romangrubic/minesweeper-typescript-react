b66a976521b473cbc57b7ef5cdcfd55ec21d6dbe
Roman Grubic <grubic.roman@gmail.com>
2020-08-15 13:57:13 +0100

    Updated button component to show number and if visible.

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
 import './button.styles.scss';
+import { CellState, CellValue } from '../../utils/utils.types';
 
-const Button: React.FC = () => {
-    return <div className='button'></div>;
+interface ButtonProps {
+    row: number;
+    col: number;
+    state: CellState;
+    value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+    const renderContent = (): React.ReactNode => {
+        if (state === CellState.visible) {
+            if (value === CellValue.bomb) {
+                return (
+                    <span role='img' aria-label='bomb'>
+                        💣
+                    </span>
+                );
+            } else if (value === CellValue.none) {
+                return null;
+            }
+            return value;
+        } else if (state === CellState.flagged) {
+            return (
+                <span role='img' aria-label='flag'>
+                    🚩
+                </span>
+            );
+        }
+        return null;
+    };
+    return (
+        <div
+            className={`button ${
+                state === CellState.visible ? 'visible' : ''
+            } value-${value}`}
+        >
+            {renderContent()}
+        </div>
+    );
 };
 
 export default Button;
