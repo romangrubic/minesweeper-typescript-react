0b6adc6b242dff4114c1c184c83eab12f957dd13
Roman Grubic <grubic.roman@gmail.com>
2020-08-16 16:24:37 +0100

    Setting props to button component.

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -7,9 +7,18 @@ interface ButtonProps {
     col: number;
     state: CellState;
     value: CellValue;
+    onClick(rowParam: number, columnParam: number): (...args: any[]) => void;
+    onContext(rowParam: number, columnParam: number): (...args: any[]) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+const Button: React.FC<ButtonProps> = ({
+    row,
+    col,
+    state,
+    value,
+    onClick,
+    onContext,
+}) => {
     const renderContent = (): React.ReactNode => {
         if (state === CellState.visible) {
             if (value === CellValue.bomb) {
@@ -36,6 +45,8 @@ const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
             className={`button ${
                 state === CellState.visible ? 'visible' : ''
             } value-${value}`}
+            onClick={onClick(row, col)}
+            onContextMenu={onContext(row, col)}
         >
             {renderContent()}
         </div>
