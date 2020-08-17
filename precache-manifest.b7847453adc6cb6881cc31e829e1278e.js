406b89a6486b77e299e2d4ba64dfa86a2ad8c3e2
Roman Grubic <grubic.roman@gmail.com>
2020-08-17 19:02:38 +0100

    Updates

---
+++ precache-manifest.b7847453adc6cb6881cc31e829e1278e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b00903682a1756792b4c27e58bb030b",
+    "url": "/minesweeper-typescript-react/index.html"
+  },
+  {
+    "revision": "c0219e00be80d7ff1d7b",
+    "url": "/minesweeper-typescript-react/static/css/main.b5a224be.chunk.css"
+  },
+  {
+    "revision": "f75e71df73b419837099",
+    "url": "/minesweeper-typescript-react/static/js/2.90aa0b02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper-typescript-react/static/js/2.90aa0b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0219e00be80d7ff1d7b",
+    "url": "/minesweeper-typescript-react/static/js/main.a558d820.chunk.js"
+  },
+  {
+    "revision": "dc60c6e9c80681a2360b",
+    "url": "/minesweeper-typescript-react/static/js/runtime-main.6ab1c48b.js"
+  }
+]);
